1e36c63ebfb92b0326f9637f0f94c5c0b17771f3
Maria Edström <mariat.edstrom@gmail.com>
2023-12-12 16:17:12 +0100

    WPF added

--- Labb2_DbFirst/Labb2_DbFirst/Program.cs
+++ Labb2_DbFirst/Labb2_DbFirst/Program.cs
@@ -1,2 +1,33 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using Labb2_DbFirst.Entities;
+
+Console.WriteLine("Hello, Database!\n=========================================\n");
+
+var db = new Labb1BookShopContext();
+
+
+var books = db.Books
+    .ToList()
+    .OrderBy(b=>b.Price);
+
+foreach (var book in books)
+{
+    Console.WriteLine($"Title: {book.Title} \nPrice: {book.Price} kr \n");
+}
+
+Console.WriteLine("\n=====================================\n");
+
+var bookNotFound = db.Books
+    .ToList()
+    .Where(b => b.Title == "Error");
+
+if (bookNotFound == null)
+{
+    Console.WriteLine("Error: Book not found");
+}
+else
+{
+    Console.WriteLine("How did this happend???");
+}
+
+
