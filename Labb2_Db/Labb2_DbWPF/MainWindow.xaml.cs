0bcca2962a6b51fdb5cabf45bd4b9b16e9ee5b11
Maria Edström <mariat.edstrom@gmail.com>
2023-12-18 20:41:28 +0100

    update/delete buttons added (not done)

--- Labb2_Db/Labb2_DbWPF/MainWindow.xaml.cs
+++ Labb2_Db/Labb2_DbWPF/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Labb2_Db.Entities;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Labb2_DbWPF
 {
@@ -17,11 +18,39 @@ namespace Labb2_DbWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static event Action ProductListChanged;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private InventoryBalance? _selectedBook;
+
+        public InventoryBalance? SelectedBook
+        {
+            get
+            {
+                return _selectedBook;
+            }
+            set
+            {
+                if (_selectedBook == value) return;
+                _selectedBook = value;
+                if (value is null)
+                {
+                    BookTitleTextBox.Text = string.Empty;
+                    BookPriceTextBox.Text = string.Empty;
+                    BookQuantityTextBox.Text = string.Empty;
+                }
+                else
+                {
+                    BookTitleTextBox.Text = _selectedBook.Title;
+                    BookPriceTextBox.Text = _selectedBook.UnitPrice.ToString();
+                    BookQuantityTextBox.Text = _selectedBook.Quantity.ToString();
+                }
+            }
+        }
 
         private void ScienceFictionBokhandelnBtn_OnClick(object sender, RoutedEventArgs e)
         {
